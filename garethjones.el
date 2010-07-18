21c76a86a031e014a903bf5a3c0d3f1cec05c9a9
Gareth Jones <gareth.e.jones@gmail.com>
2010-07-18 11:10:53 -0500

    use inconsolata by default, and disable the command key for system commands

---
+++ garethjones.el
@@ -0,0 +1,6 @@
+;; use inconsolata
+(set-face-attribute 'default nil
+                    :family "inconsolata" :height 160)
+
+;; dont send command to mac-os
+(setq  mac-pass-command-to-system nil)
