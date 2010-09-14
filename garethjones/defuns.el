fc8ec9a9ef610b405a04ca26d7d4a0d924924e04
Gareth Jones <gareth.e.jones@gmail.com>
2010-09-13 20:56:47 -0500

    adding fullscreen function

--- garethjones/defuns.el
+++ garethjones/defuns.el
@@ -7,4 +7,14 @@
   (yank)
   (beginning-of-line)
   (indent-according-to-mode))
-  
+
+(defun toggle-fullscreen (&optional f)
+      (interactive)
+      (let ((current-value (frame-parameter nil 'fullscreen)))
+           (set-frame-parameter nil 'fullscreen
+                                (if (equal 'fullboth current-value)
+                                    (if (boundp 'old-fullscreen) old-fullscreen nil)
+                                    (progn (setq old-fullscreen current-value)
+                                           'fullboth)))))
+(global-set-key [f7] 'toggle-fullscreen)
+
