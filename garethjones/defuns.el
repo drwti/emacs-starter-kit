bb3d1f96e2d197aa527cd8b03000678d37315be9
Gareth Jones <gareth.e.jones@gmail.com>
2010-09-22 20:20:30 -0500

    adding scratch function

--- garethjones/defuns.el
+++ garethjones/defuns.el
@@ -7,4 +7,29 @@
                                     (progn (setq old-fullscreen current-value)
                                            'fullboth)))))
 
+(defvar *clojure-scratch-dir* "~/sandbox/clojure/clj-scratch"
+  "Location of a scratch project for quickly playing with clojure stuff")
+
+(defun scratch-clojure ()
+  (interactive)
+  (if (file-exists-p *clojure-scratch-dir*)
+      (progn
+        (cd *clojure-scratch-dir*)
+        (elein-swank)
+        (find-file (concat *clojure-scratch-dir* "/src/clj_scratch/core.clj")))
+    (message "make sure you have your clj-scratch project set up sucker")))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
