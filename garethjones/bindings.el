584ca695298d89d25f74222b5db39844ca36db61
Gareth Jones <gareth.e.jones@gmail.com>
2010-10-06 12:10:26 -0500

    added steve yegges suggested keybindings

--- garethjones/bindings.el
+++ garethjones/bindings.el
@@ -2,3 +2,13 @@
 (setq mac-pass-command-to-system nil)
 
 (global-set-key [f7] 'toggle-fullscreen)
+
+;; steve yegges's suggested keybindings
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-c\C-m" 'execute-extended-command)
+
+(global-set-key "\C-w" 'backward-kill-word)
+(global-set-key "\C-x\C-k" 'kill-region)
+(global-set-key "\C-c\C-k" 'kill-region)
+
+(global-set-key [f5] 'call-last-kbd-macro)
