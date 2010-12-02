c0280e66019478a224029247f392f33217fdddc4
Gareth Jones <gareth.e.jones@gmail.com>
2010-12-02 16:20:37 -0600

    updated to use latest package.el and added technomancy's repo

--- default-profile.el
+++ default-profile.el
@@ -1,6 +1,10 @@
 ;; dir to store all extra extensions
 (setq vendor-dir (concat dotfiles-dir "/vendor"))
 
+;; adding technomancy's package archive
+(add-to-list 'package-archives
+                 '("technomancy" . "http://repo.technomancy.us/emacs/") t)
+
 ;; add my vendor dir to load path
 (add-to-list 'load-path vendor-dir)
 
