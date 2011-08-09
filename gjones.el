f7f6276552080d626067fc0fb93f1513892174b6
Gareth Jones <gareth.e.jones@gmail.com>
2011-08-09 15:22:23 -0500

    gaz - turn off hl-line-mode as it is annoying

--- gjones.el
+++ gjones.el
@@ -11,3 +11,6 @@
 (setq jabber-chat-buffer-show-avatar nil)
 (setq jabber-vcard-avatars-retrieve nil)
 (setq jabber-roster-line-format " %c %-25n %u %-8s (%r)")
+
+;; turn off stupid line highlighting 9_9
+(hl-line-mode nil)
