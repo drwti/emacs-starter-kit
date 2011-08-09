e8cbec8463bcb911e334afff2a12bb428f8fffc2
Gareth Jones <gareth.e.jones@gmail.com>
2011-08-09 08:42:14 -0500

    gaz - adding jabber

--- gjones.el
+++ gjones.el
@@ -1,1 +1,13 @@
-default-profile.el
+(load "default-profile.el")
+
+(add-to-list 'load-path (concat vendor-dir "/emacs-jabber-0.8.0"))
+(require 'jabber-autoloads)
+
+(setq jabber-account-list
+    '(("gareth.e.jones@gmail.com" 
+       (:network-server . "talk.google.com")
+       (:connection-type . ssl))))
+
+(setq jabber-chat-buffer-show-avatar nil)
+(setq jabber-vcard-avatars-retrieve nil)
+(setq jabber-roster-line-format " %c %-25n %u %-8s (%r)")
